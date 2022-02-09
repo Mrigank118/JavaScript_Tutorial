098a34e68212de7145cea0dba5dd039db45f1160
Mrigank <mrigank.11.8@gmail.com>
2022-02-09 20:16:16 +0530

    Initial Commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,163 @@
+/*
+let js='amazing';
+  
+   console.log(40+1);
+   console.log('jonas');
+   console.log(11.8);
+
+   let firstName= 'Mrigank';
+   console.log(firstName);
+
+  
+  let javascriptIsFun = true;
+  
+  console.log(javascriptIsFun);
+  console.log(typeof true);
+  console.log(typeof 23);
+
+  javascriptIsFun = 'yes!';
+  console.log(typeof javascriptIsFun);
+
+  let year;sisAge);
+
+console.log(myAge *2, myAge / 2, 2**3 ,3**3);
+
+console.log(myAge > sisAge);
+
+  console.log(year);
+  console.log(typeof year);
+
+  console.log(typeof null);
+  
+  
+const myAge = 2021-2004;
+const sisAge= 2021-2000;
+console.log(myAge, 
+
+
+const firstName='Mrigank' ;
+const job= 'WebDeveloper';
+const birthYear = 2004;
+const year = 2022;
+ 
+const Me = "I'm" + " " + firstName + " " + "a" + " " + job + " " + "I was born in" + " " + birthYear + "." ;
+ 
+ 
+
+const meNew= `I'm  ${firstName}, a ${job}, born in ${birthYear}.`;
+console.log(meNew); 
+console.log(`Just a regular string`);
+
+
+
+const age=18;
+if(age===18)
+console.log('You are an adult');
+
+const fav=prompt("whats your fav no.")
+console.log(fav);
+
+if(fav===23)
+console.log('23 is amazing');
+
+
+const day ='monday';
+switch(day)
+{
+  case 'monday':
+  console.log('I plan my couse sturcutre')
+  ;
+}
+const calcAge2 =function (birthYear){
+  return 2037- birthYear;
+}
+
+
+const calcAge3 = birthYear => 2037-birthYear;
+const age3 = calcAge3(1991);
+
+
+const calcAvg = function (x1,x2,x3){
+  return x1+x2+x3/3;
+}
+const Koalas = calcAvg(4,5,6)
+console.log(Koalas);
+
+const Dolphines = calcAvg(9,7,8)
+console.log(Dolphines);
+
+
+
+const frnd1 ='Mrigank';
+const frnd2 ='Peter';
+const frnd3 ='John';
+const frnds =['Mrigank','Peter','John'] ;
+frnds.push('Jay');
+console.log(frnds);
+*/
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+function logger()
+{
+    console.log('My name is Mrigank');
+}
+
+logger();
+logger();
+logger();
+logger();
+logger();
